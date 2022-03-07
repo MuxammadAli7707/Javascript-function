2aa740ea4c20061da4c31cc838dde816ecdeb818
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-03-08 01:19:08 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,111 @@
+// Array 1  n gacha bolgan toq sonlarni massivga yozish
+
+// let n = 5;
+// let massArray = [];
+// for (let i = 1; i <= n * 2 ; i++){
+//   if (i % 2 != 0) {
+//     massArray[massArray.length] = i;
+//   }
+// }
+// console.log(massArray);
+
+
+// Array 2  2 sonni darajalarini chiqarish
+// let n = 4;
+// let a = 2;
+// let massArray = [];
+// let b = [];
+// for (let i = 1; i <= n; i++) {
+//   massArray[i] = i;
+//   massArray[i] *= a;
+//   b [b.length] = massArray[i];
+// }
+// console.log(b);
+
+
+// Array 3 n gacha arifmetic progressiya
+// let n = 10;
+// let a = 1;
+// let b = 2;
+// let massArray = [];
+// for (let i = 1; i <= n ; i++) {
+//   massArray[massArray.length] = a;
+//   a += b;
+// }
+// console.log(massArray);
+
+
+// Array 4  geometric progressiya
+// let n = 10;
+// let a = 1;
+// let b = 2;
+// let massArray = [];
+// for (let i = 1; i <= n ; i++) {
+//   massArray[massArray.length] = a;
+//   a *= b;
+// }
+// console.log(massArray);
+
+// Array 5 fibonachi sonlar
+// let n = 8;
+// let a = 0;
+// let b = 1;
+// let c = 0;
+// let newArray = [];
+
+// for(let i = 1; i <= n; i++){
+//   c = a + b;
+//   a = b;
+//   b = c;
+//   newArray[newArray.length] = c;
+// }
+
+// console.log(newArray);
+
+// Array 7 teskari chiqarish
+// let massArray = [1,2,3,4,5,6,7,8,9,10];
+// console.log(massArray.reverse());
+
+// Array 8 toq sonlar va ularni soni
+// let n = 10;
+// let massArray = [];
+// let count = 0;
+// for(let i = 1; i <= n; i++){
+//   if(i % 2 != 0){
+//     massArray[massArray.length] = i;
+//     count++;
+//   } 
+// }
+
+// console.log(massArray, `${count} ta toq son`);
+
+// Array 9 juft sonlar va ularni soni
+// let n = 10;
+// let massArray = [];
+// let count = 0;
+// for(let i = n; i >= 1; i--){
+//   if(i % 2 == 0){
+//     massArray[massArray.length] = i;
+//     count++;
+//   } 
+// }
+
+// console.log(massArray, `${count} ta juft son`);
+
+// Array 10 juft sonlar va toq sonlar
+
+// let newArray = [2, 3, 7, 11, 16, 20, 13, 18];
+// let newCount = [];
+// let newAdd = [];
+
+// for(let i = 0; i < newArray.length; i++){
+//   if(newArray[i] % 2 != 0){
+//     newCount[newCount.length] = newArray[i];
+//   } else {
+//     newAdd[newAdd.length] = newArray[i];
+//   }
+  
+// } 
+// console.log(newCount);
+// console.log(newAdd);
+
---
+++ js/main.js
@@ -0,0 +1,67 @@
+// Arrow function
+
+// let age = prompt("What is your age", 18);
+
+// let welcome =
+//   age >= 18
+//     ? () => alert("Xush kelibsiz 😂")
+//     : () => alert("Salomat bo'ling 🤢");
+
+// welcome();
+
+// let sort = (a, b, c) => {
+//   if(a <= b && a <= c){
+//     if(b <= c){
+//       console.log(a,b,c);
+//     } else {
+//       console.log(a,c,b);
+//     }
+//   } else if(b <= a && b <= c){
+//     if(a <= c){
+//       console.log(b,a,c);
+//     } else {
+//       console.log(b,c,a);
+//     }
+//   } else{
+//     console.log(c,b,a);
+//   }
+// }
+
+// let a = 4, b = 2, c = 3;
+// sort(a, b, c);
+
+
+// let newArray = [15, 20, 30, 55, 69, 12]
+// for(let i = 0; i < newArray.length; i++){
+//   if(newArray[i] % 2 != 0){
+//     console.log(newArray[i]);
+//   }
+// }
+
+// let newArray = [2, 3, 7, 11, 16]
+// for(let i = 0; i < newArray.length; i++){
+//   let count = 0;
+//   for(let j = 2; j < i; j++){
+//     if(newArray[i] % j == 0){
+//       count++;
+//     }
+//   }
+//   if(count == 0){
+//     console.log(newArray[i]);
+//   }
+// }
+
+// let newArray = [2, 3, 7, 11, 16];
+// let newCount = [];
+// let newAdd = [];
+
+// for(let i = 0; i < newArray.length; i++){
+//   if(newArray[i] % 2 != 0){
+//     newCount[newCount.length] = newArray[i];
+//   } else {
+//     newAdd[newAdd.length] = newArray[i];
+//   }
+  
+// } 
+// console.log(newCount);
+// console.log(newAdd);
